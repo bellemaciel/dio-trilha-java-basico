3ee922d739d316dded450eb43c89a55f14efdf53
bellemaciel <isabely.12@hotmail.com>
2024-06-03 21:48:32 -0300

    Mudanças no README + Estudo do break e do continue.

---
+++ ControleDeFluxo/src/BreakContinue.java
@@ -0,0 +1,26 @@
+
+public class BreakContinue{
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <=5; i++){
+            if(i == 3)
+                break;// interrompe o for
+            System.out.println(i);
+        }
+
+        System.out.println("\n Fim do break - início do continue: \n");
+
+        for(int i = 1; i <=5; i++){
+            if(i == 3)
+                continue;// muda o fluxo, volta ao for sem imprimir o número 3
+            System.out.println(i);
+        }
+    }
+
+
+
+
+
+
+
+}
